ff785272258d2ea6939c8c402f44e30339d131a9
Daniel Bautista <dbautista9015@gmail.com>
2024-09-10 21:36:29 -0700

    Add comment to verify conflicting ports being used

--- lua/custom/plugins/live-server.lua
+++ lua/custom/plugins/live-server.lua
@@ -5,6 +5,8 @@ return {
     cmd = { 'LiveServerStart', 'LiveServerStop' },
     config = function()
       require('live-server').setup {
+        -- Make sure to check if any ports are being used before configuring.
+        -- Ex) lsof -i :9000
         args = { '--browser=google chrome', '--port=9000' },
       }
     end,
